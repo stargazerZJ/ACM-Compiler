59e130f9ca553c5566081700313c5378cac386a8
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2024-08-13 08:44:55 +0800

    fix: encoding = utf-8

--- syntax_checker.py
+++ syntax_checker.py
@@ -394,7 +394,7 @@ if __name__ == '__main__':
     # test_file_path = "./testcases/sema/const-array-package/const_array1.mx"
     # test_file_path = "./testcases/sema/scope-package/scope-1.mx"
     # input_stream = antlr4.FileStream(test_file_path, encoding='utf-8')
-    input_stream = antlr4.StdinStream()
+    input_stream = antlr4.StdinStream(encoding='utf-8')
     lexer = MxLexer(input_stream)
     parser = MxParser(antlr4.CommonTokenStream(lexer))
 
