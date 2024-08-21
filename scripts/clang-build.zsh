b49faeb2e9eba1833f39bef9b052507abada8ad1
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2024-08-21 16:06:14 +0800

    test(IR): set clang version to 19

--- scripts/clang-build.zsh
+++ scripts/clang-build.zsh
@@ -4,10 +4,10 @@ cd "${0:h}"/.. || exit
 
 mkdir -p ../clang_generated
 
-clang -emit-llvm \
+clang-19 -emit-llvm \
   -fno-builtin-printf -fno-builtin-memcpy -fno-builtin-malloc -fno-builtin-strlen\
   -O3 --target=riscv32 \
   builtin.c -S \
   -o clang_generated/builtin.ll
 
-llc --march=riscv32 -O3 -mattr=+m,+a clang_generated/builtin.ll -o clang_generated/builtin.s
+llc-19 --march=riscv32 -O3 -mattr=+m,+a clang_generated/builtin.ll -o clang_generated/builtin.s
