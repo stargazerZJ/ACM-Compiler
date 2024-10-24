08695cb856e3236e6b03ff679d8d0c008a8ce168
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2024-10-24 10:12:45 +0800

    fix(mir): non-self tall call with more than 8 params

--- asm_builder.py
+++ asm_builder.py
@@ -311,6 +311,8 @@ class ASMBuilder(ASMBuilderUtils):
 
                 param_count = len(cmd.func.param_types)
 
+                assert param_count <= 8, "Tail call with more than 8 parameters"
+
                 param_to = self.prepare_params(param_count)
                 param_from = self.prepare_var_from(cmd.var_use)
                 block.add_cmd(*self.rearrange_operands(
